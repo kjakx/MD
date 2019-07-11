944245958bd6d9a902dd4b99b578887967f41ba7
kjakx <43692824+kjakx@users.noreply.github.com>
2019-07-11 22:30:13 +0900

    Update Molecule.hpp

--- include/Molecule.hpp
+++ include/Molecule.hpp
@@ -20,26 +20,26 @@ public:
 	tuple<double, double, double> get_p();
 }
 
-inline void set_q(double qx, double qy, double qz)
+inline void Molecule::set_q(double qx, double qy, double qz)
 {
 	this->qx = qx;
 	this->qy = qy;
 	this->qz = qz;
 }
 
-inline void set_p(double px, double py, double pz)
+inline void Molecule::set_p(double px, double py, double pz)
 {
 	this->px = px;
 	this->py = py;
 	this->pz = pz;
 }
 
-inline tuple<double, double, double> get_q()
+inline tuple<double, double, double> Molecule::get_q()
 {
 	return forward_as_tuple(qx, qy, qz);
 }
 
-inline tuple<double, double, double> get_p()
+inline tuple<double, double, double> Molecule::get_p()
 {
 	return forward_as_tuple(px, py, pz);
 }
