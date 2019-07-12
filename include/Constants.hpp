f2cd90c566abe17110cb997349a6e07de56f289f
kjakx <43692824+kjakx@users.noreply.github.com>
2019-07-12 19:45:51 +0900

    Update Constants.hpp

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -6,4 +6,4 @@
 const long double CUTOFF_R2 = 2.0;
 const long double CUTOFF_R6 = pow(CUTOFF_R2, 3);
 const long double CUTOFF_R12 = pow(CUTOFF_R6, 2);
-const long double U0 = 4 * (1.0 / CUTOFF_R12 - 1.0 / CUTOFF_R6);
+const long double U0 = - 4 * (1.0 / CUTOFF_R12 - 1.0 / CUTOFF_R6);
