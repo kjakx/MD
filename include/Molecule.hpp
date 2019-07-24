0ec399cde8aa6fad3493201e896f71499e6fd154
kjakx <43692824+kjakx@users.noreply.github.com>
2019-07-24 14:44:47 +0900

    Update Molecule.hpp

--- include/Molecule.hpp
+++ include/Molecule.hpp
@@ -19,14 +19,6 @@ public:
 	double px, py, pz;
 // method:
 	Molecule(double qx, double qy, double qz);
-	~Molecule();
-	// setter
-	//void set_q(double qx, double qy, double qz);
-	//void set_p(double px, double py, double pz);
-
-	// getter
-	//tuple<double, double, double> get_q();
-	//tuple<double, double, double> get_p();
 	double get_kinetic_energy();
 	void interact_with(Molecule& that);
 }
@@ -39,31 +31,7 @@ inline Molecule(double qx, double qy, double qz)
 	this->qz = qz;
 	init_MB_velocity(this);
 }
-/*
-inline void Molecule::set_q(double qx, double qy, double qz)
-{
-	this->qx = qx;
-	this->qy = qy;
-	this->qz = qz;
-}
 
-inline void Molecule::set_p(double px, double py, double pz)
-{
-	this->px = px;
-	this->py = py;
-	this->pz = pz;
-}
-inline tuple<double, double, double> Molecule::get_q()
-{
-	// Since I would like to write in a line, I used tuple.
-	return forward_as_tuple(qx, qy, qz);
-}
-
-inline tuple<double, double, double> Molecule::get_p()
-{
-	return forward_as_tuple(px, py, pz);
-}
-*/
 inline double Molecule::get_kinetic_energy()
 {
 	double k = 0;
@@ -86,9 +54,11 @@ inline void Molecule::interact_with(Molecule& that)
 	fy = f * ry;
 	fz = f * rz;
 	// interaction!
+	// action
 	this.px += fx * dt * 0.5;
 	this.py += fy * dt * 0.5;
 	this.pz += fz * dt * 0.5;
+	// reaction
 	that.px -= fx * dt * 0.5;
 	that.py -= fy * dt * 0.5;
 	that.pz -= fz * dt * 0.5;
