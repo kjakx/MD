2d42c65f457517097e4502b6b6835180ef33b7ae
kjakx <43692824+kjakx@users.noreply.github.com>
2019-07-26 16:10:05 +0900

    Create Molecule.cpp

---
+++ src/Molecule.cpp
@@ -0,0 +1,64 @@
+#include "Molecule.hpp"
+
+using namespace std;
+
+Molecule(double qx, double qy, double qz)
+{
+	mass = m0 / u_m;
+	this->qx = qx;
+	this->qy = qy;
+	this->qz = qz;
+	init_MB_velocity(this);
+}
+
+tuple<double, double, double> Md::r_xyz_to(Molecule& that)
+{
+	double rx, ry, rz;
+	// components of distance between i-j
+	rx = that.qx - this.qx;
+	ry = that.qy - this.qy;
+	rz = that.qz - this.qz;
+	// periodic boundary condition
+	correct_distance(rx, ry, rz);
+	return forward_as_tuple(rx, ry, rz);
+}
+
+double Molecule::r2_to(Molecule& that)
+{
+	double rx, ry, rz;
+	tie(rx, ry, rz) = this->r_xyz_to(that);
+	r2 = pow(rx, 2) + pow(ry, 2) + pow(rz, 2);
+	return r2;
+}
+
+double Molecule::get_kinetic_energy()
+{
+	double k = 0;
+	k += pow(px, 2);
+	k += pow(py, 2);
+	k += pow(pz, 2);
+	return k * 0.5;
+}
+
+void Molecule::interact_with(Molecule& that)
+{
+        double rx, ry, rz;
+	double f, fx, fy, fz;
+	// components of distance between i-j
+	tie(rx, ry, rz) = this->r_xyz_to(that);
+	// calculate force between i-j with derivative of LJ potential
+	f = VDW_forces_between(this, that);
+	// components of forces between i-j
+	fx = f * rx;
+	fy = f * ry;
+	fz = f * rz;
+	// interaction!
+	// action
+	this.px += fx * dt * 0.5;
+	this.py += fy * dt * 0.5;
+	this.pz += fz * dt * 0.5;
+	// reaction
+	that.px -= fx * dt * 0.5;
+	that.py -= fy * dt * 0.5;
+	that.pz -= fz * dt * 0.5;
+}
